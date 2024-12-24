957c1f8ee45f2fa576a331ca5a9d40b3e9449a16
ShahFaisalkhan <shahfaisalkhan1993@gmail.com>
2024-12-24 10:44:44 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,7 +35,7 @@ const Home = () => {
       const queryString = query.join('&'); // Join the parts with "&" and converts it to string
       // console.log("query string in home.js is",queryString)
       // const { data } = await axios.get(`http://localhost:5000/api/items?${queryString}`);
-      const { data } = await axios.get(`https://d505e071-8ecb-4b8c-a1c7-41f19962102c-00-2e9f2fpl227us.kirk.replit.dev/api/items?${queryString}`);
+      const { data } = await axios.get(`https://lost-and-found-v2-backend-production.up.railway.app/api/items?${queryString}`);
 
         // const { data } = await axios.get(`http://localhost:5000/api/items?${query}`);
         setItems(data.items || []);
@@ -135,7 +135,7 @@ const Home = () => {
             {item.imageUrl ? (
     <img
       // src={`http://localhost:5000${item.imageUrl}`}
-      src={`https://https://d505e071-8ecb-4b8c-a1c7-41f19962102c-00-2e9f2fpl227us.kirk.replit.dev${item.imageUrl}`}
+      src={`https://https://lost-and-found-v2-backend-production.up.railway.app${item.imageUrl}`}
       alt={item.title}
       className="card-img-top"
       style={{ height: '200px', objectFit: 'cover' }}
